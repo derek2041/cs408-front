5d4f8be4da0a8273fabb3bdb0a976837e7491605
Ubuntu <ubuntu@ip-172-31-22-191.us-east-2.compute.internal>
2019-08-29 04:13:15 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavigationBar from './NavigationBar';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavigationBar />
+
+        <Switch>
+
+          <Route path="/one/" exact render={ () => {
+            return (
+              <img
+                src="https://s3-media1.fl.yelpcdn.com/bphoto/AELV_t3DBJaPZCsQOGcsRg/o.jpg"
+                alt=""
+              />
+            );
+          }} />
+
+          <Route path="/two/" exact render={ () => {
+            return (
+              <img
+                src="https://s3-media2.fl.yelpcdn.com/bphoto/XRTwKMvLOFswl6bywAu92g/o.jpg"
+                alt=""
+              />
+            );
+          }} />
+
+          <Route path="/three/" exact render={ () => {
+            return (
+              <img
+                src="https://s3-media3.fl.yelpcdn.com/bphoto/dJzIEcoehgN2K1vmZEioHQ/o.jpg"
+                alt=""
+              />
+            );
+          }} />
+
+          <Route path="/four/" exact render={ () => {
+            return (
+              <img
+                src="https://s3-media2.fl.yelpcdn.com/bphoto/nA9K90rczbClXXjxmM4Ezg/o.jpg"
+                alt=""
+              />
+            );
+          }} />
+
+        </Switch>
+      </Router>
     </div>
   );
 }
