6d0fa594e9f7913498f0e4d99db021400ed772e7
Ubuntu <ubuntu@ip-172-31-22-191.us-east-2.compute.internal>
2019-10-09 00:46:16 +0000

    Changed settings for production build: no source map, no console.log()

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const noop = () => {}
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log = noop;
+  console.warn = noop;
+  console.error = noop;
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
